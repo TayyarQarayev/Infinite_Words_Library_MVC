f9772ba9abf93fead02d48bb16e789975484a1d0
TayyarQarayev <qarayevtyyar@gmail.com>
2024-05-13 01:20:09 +0400

    Add project files.

---
+++ Infinite_Words_Library_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Infinite_Words_Library_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
