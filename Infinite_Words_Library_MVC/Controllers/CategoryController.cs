922edc019b0837d2fc6a762f5c8bca14dec8f1c0
TayyarQarayev <qarayevtyyar@gmail.com>
2024-05-15 17:00:51 +0400

    First commit

---
+++ Infinite_Words_Library_MVC/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Infinite_Words_Library_MVC.Data;
+using Infinite_Words_Library_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infinite_Words_Library_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Catagories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name","Name cannot be equal to displayOrder");
+            }
+            if (ModelState.IsValid) 
+            {
+                _db.Catagories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            } 
+            return View();
+        }
+    }
+}
